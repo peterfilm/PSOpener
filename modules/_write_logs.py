cda1743587fb1f0ce8f7837a7cc04a5bd22b66d7
Peter Mikhaylutsa <petercash@mail.ru>
2023-09-03 10:21:54 +0300

    fixed date problem

--- modules/_write_logs.py
+++ modules/_write_logs.py
@@ -3,11 +3,10 @@ import os
 
 from datetime import datetime
 
-current_datetime = datetime.now()
-formatted_datetime = current_datetime.strftime("%Y.%m.%d - %H:%M")
-
 
 def write_log(filename_path, text, flag):
+    current_datetime = datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y.%m.%d - %H:%M")
     if conf['COMMENT_PATH']:
         path = os.path.join(conf['COMMENT_PATH'], 'log.txt')
     else:
